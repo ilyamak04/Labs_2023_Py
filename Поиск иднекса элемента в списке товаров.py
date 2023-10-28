8efd4abb89895416f8bfba4a5fc1ac1171a5a0e0
ilyamak04 <147930568+ilyamak04@users.noreply.github.com>
2023-10-28 18:59:12 +0300

    Лабораторная работа 3

---
+++ Поиск иднекса элемента в списке товаров.py
@@ -0,0 +1,15 @@
+def firtst_elem(items, item):
+    for i in range(len(items)):
+        if items[i] == item:
+            return i
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = firtst_elem(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
